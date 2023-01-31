329c7b1317715fafa9c3fbc573b4224c1f4a6314
LucDuong1720 <lucduong1720@gmail.com>
2023-01-31 14:22:54 +0700

    module2

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10; // Khai báo biến i là kiểu số nguyên có giá trị là 10
+        float f = 20.5f; // Khai báo biến f là kiểu số thực có giá trị là 20.5
+        double d = 20.5d; // Khai báo biến d là kiểu số thực có giá trị là 20.5
+        boolean b = true; // Khai báo biến b là kiểu boolean có giá trị true
+        char c = 'a'; // Khai báo biến c có kiểu là char có giá trị là ký tự a
+        String s = "Hà Nội"; // Khai báo biến s có giá trị là String có giá trị chuỗi Hà Nội
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}

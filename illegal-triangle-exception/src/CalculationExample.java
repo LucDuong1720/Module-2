f23bb56f8e99b9efdbf6b4879990d683f8cdd2a1
LucDuong1720 <lucduong1720@gmail.com>
2023-02-20 14:18:28 +0700

    module2

---
+++ illegal-triangle-exception/src/CalculationExample.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CalculationExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hãy nhập a: ");
+        int a = scanner.nextInt();
+        System.out.println("Hãy nhập b: ");
+        int b = scanner.nextInt();
+        System.out.println("Hãy nhập c: ");
+        int c = scanner.nextInt();
+
+        CalculationExample calc = new CalculationExample();
+        calc.calculate(a,b,c);
+    }
+
+    private void calculate(int a, int b, int c) {
+        try {
+            if (a < 0 || b < 0 || c < 0) {
+               throw new IllegalTriangleException("Các cạnh của tam giác phải là số dương");
+            }
+            if (a + b <= c || a + c <= b || c + b <= a) {
+                throw new IllegalTriangleException("Tổng 2 cạnh của tam giác phải lớn hơn cạnh còn lại");
+            }
+            System.out.println("Các cạnh của tam giác hợp lệ.");
+        } catch (IllegalTriangleException e) {
+            System.out.println("Lỗi: " + e.getMessage());
+        }
+    }
+}

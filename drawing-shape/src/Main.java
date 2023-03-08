f81c0842be38188393cd020980c5e7faea7f2e5f
LucDuong1720 <lucduong1720@gmail.com>
2023-03-08 17:23:56 +0700

    module2

---
+++ drawing-shape/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+        while (choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle");
+            System.out.println("3. Print isosceles triangle");
+            System.out.println("4. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    for (int i = 1; i <= 3; i++) {
+                        for (int j = 1; j <= 7; j++) {
+                            System.out.print("*  ");
+                        }
+                        System.out.println("");
+                    }
+                break;
+                case 2:
+                    for (int i = 1; i <= 6; i++) {
+                        for (int j = 1; j < i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println("");
+                    }
+                    System.out.println("");
+                    for (int i = 5; i >= 1; i-- ) {
+                        for (int j = 1; j <=i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println("");
+                    }
+                case 3:
+                    for (int i = 6; i > 0; i--) {
+                        for (int j = 1; j <= 6; j++) {
+                            if (j >= i) {
+                                System.out.print("*   ");
+                            } else {
+                                System.out.print("  ");
+                            }
+                        }
+                        System.out.println("");
+                    }
+                }
+            }
+        }
+    }

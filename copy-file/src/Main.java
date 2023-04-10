7816a3abcbb09cc10a34842d3d5be1d3b33388ce
LucDuong1720 <lucduong1720@gmail.com>
2023-04-10 08:30:05 +0700

    module

--- copy-file/src/Main.java
+++ copy-file/src/Main.java
@@ -1,44 +1,27 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
-        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-    }
-    public static void copyFileUsingStream(File source, File dest) throws IOException {
-        InputStream is = null;
-        OutputStream os = null;
+    public static void main(String[] args) throws FileNotFoundException {
         try {
-            is = new FileInputStream(source);
-            os = new FileOutputStream(dest);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
+            FileReader fileReader = new FileReader("./source.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            int line = 0;
+            FileWriter fileWriter = new FileWriter("./target.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            while ((line = bufferedReader.read()) != -1) {
+                try {
+                    bufferedWriter.write(line);
+                } catch (IOException ioException) {
+                    System.out.println("Quang Ế");
+                }
             }
-        } finally {
-            is.close();
-            os.close();
-        }
-    }
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.printf("Enter source file:");
-        String sourcePath = in.nextLine();
-        System.out.printf("Enter destination file:");
-        String destPath = in.nextLine();
-
-        File sourceFile = new File(sourcePath);
-        File destFile = new File(destPath);
-        try {
-            copyFileUsingJava7Files(sourceFile, destFile);
-            System.out.printf("Copy completed");
-        } catch (IOException ioe) {
-            System.out.printf("Can't copy that file");
-            System.out.printf(ioe.getMessage());
+            bufferedWriter.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
+
